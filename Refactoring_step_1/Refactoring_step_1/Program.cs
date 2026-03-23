ddc9d0da6b70aa5a17a979d502063161c0171246
blaKy <sashaegorov.07@mail.ru>
2026-03-23 15:41:30 +0300

    step_1

---
+++ Refactoring_step_1/Refactoring_step_1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internet_Shop_do_refactora
+{
+    internal class Program
+    {
+        public class UserService
+        {
+            private readonly SqlConnection _connection;
+            public UserService(SqlConnection connection)
+            {
+                _connection = connection;
+            }
+            //проблемный метод:получение пользователей с их заказами
+            public List<object> GetUsersWithOrders()
+            {
+                var result = new List<object>();
+                //Уязвимость SQL иньекции
+                var usersCmd = new SqlCommand("SELECT * FROM Users", _connection);
+                var usersReader = usersCmd.ExecuteReader();
+                while (usersReader.Read())
+                {
+                    var userId = usersReader.GetInt32(0);
+                    var userName = usersReader.GetString(1);
+                    //проблема N+1: запрос в цикле
+                    var ordersCmd = new SqlCommand($"SELECT * FROM Orders where UserId={userId}", _connection);
+                    var ordersReader = ordersCmd.ExecuteReader();
+                    var orders = new List<object>();
+                    while (ordersReader.Read())
+                    {
+                        orders.Add(new { OrderId = ordersReader.GetInt32(0), Total = ordersReader.GetDecimal(1) });
+                    }
+                    ordersReader.Close();
+                    result.Add(new
+                    {
+                        UserID = userId,
+                        Name = userName,
+                        Orders = orders
+                    });
+                }
+                usersReader.Close();
+                return result;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Refactoring_step_1/Refactoring_step_1/step1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Internet_shaop_do_refactora1
+{
+    internal class step1
+    {
+        string _connectionString;
+
+        public class UserWithOrdersDto
+        {
+            public int UserId { get; set; }
+            public string Name { get; set; }
+            public List<OrderDto> Orders { get; set; }
+        }
+
+        public class OrderDto
+        {
+            public int OrderId { get; set; }
+            public decimal Total { get; set; } // Исправлено: Total должен быть decimal, а не int
+        }
+
+        public List<UserWithOrdersDto> GetUserWithOrders()
+        {
+            var result = new List<UserWithOrdersDto>();
+
+            // 1 using для автоматического закрытия
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                // 2 защита от sql инъекций (параметризированный запрос)
+                const string usersSql = "SELECT Id, Name FROM Users";
+
+                using (var userCmd = new SqlCommand(usersSql, connection))
+                using (var usersReader = userCmd.ExecuteReader())
+                {
+                    while (usersReader.Read())
+                    {
+                        var userId = usersReader.GetInt32(usersReader.GetOrdinal("Id"));
+                        var userName = usersReader.GetString(usersReader.GetOrdinal("Name"));
+
+                        // Все еще N+1 проблема, но ресурсы теперь закрываются
+                        const string orderSql = "SELECT Id, Total FROM Orders WHERE UserID=@UserId";
+                        using (var ordersCmd = new SqlCommand(orderSql, connection))
+                        {
+                            ordersCmd.Parameters.AddWithValue("@userId", userId);
+                            using (var ordersReader = ordersCmd.ExecuteReader())
+                            {
+                                var orders = new List<OrderDto>();
+                                while (ordersReader.Read())
+                                {
+                                    orders.Add(new OrderDto
+                                    {
+                                        OrderId = ordersReader.GetInt32(ordersReader.GetOrdinal("Id")),
+                                        Total = ordersReader.GetDecimal(ordersReader.GetOrdinal("Total"))
+                                    });
+                                }
+
+                                result.Add(new UserWithOrdersDto
+                                {
+                                    UserId = userId,
+                                    Name = userName,
+                                    Orders = orders
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
